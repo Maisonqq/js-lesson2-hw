0f56a8694cfa4e11813adbb8070724ff17b9c8d2
Ruslan Klyuchka <1701003207602@mail.ru>
2020-12-09 21:10:54 +0300

    added 16

--- index.js
+++ index.js
@@ -81,12 +81,16 @@
 // }
 
 //Пятнадцатое задание
-let value = prompt("Введите число");
+// let value = prompt("Введите число");
 
-if (value > 0) {
-  alert(1);
-} else if (value < 0) {
-  alert(-1);
-} else {
-  alert(0);
-}
+// if (value > 0) {
+//   alert(1);
+// } else if (value < 0) {
+//   alert(-1);
+// } else {
+//   alert(0);
+// }
+
+//Шестнадцатое задание
+// let result;
+// result = a + b < 4 ? "Мало" : "Много"; // <------- Вот это вообще не понял
