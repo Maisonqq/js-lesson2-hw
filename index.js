da1cd37c86a2d6b32c22a0f40e2be4eb1952855b
Ruslan Klyuchka <1701003207602@mail.ru>
2020-12-09 20:46:54 +0300

    added 15

--- index.js
+++ index.js
@@ -79,3 +79,14 @@
 // } else {
 //   alert("Не знаете? ECMAScript");
 // }
+
+//Пятнадцатое задание
+let value = prompt("Введите число");
+
+if (value > 0) {
+  alert(1);
+} else if (value < 0) {
+  alert(-1);
+} else {
+  alert(0);
+}
